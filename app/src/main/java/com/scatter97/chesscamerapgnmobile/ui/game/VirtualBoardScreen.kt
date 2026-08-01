7c49d5a594a79743be15ae72c1fad2b96b296c95
Joshua Wang <133073535+Scatter97@users.noreply.github.com>
2026-08-01 19:58:09 -0400

    Fix virtual board Compose layout import

--- app/src/main/java/com/scatter97/chesscamerapgnmobile/ui/game/VirtualBoardScreen.kt
+++ app/src/main/java/com/scatter97/chesscamerapgnmobile/ui/game/VirtualBoardScreen.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -30,7 +28,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
