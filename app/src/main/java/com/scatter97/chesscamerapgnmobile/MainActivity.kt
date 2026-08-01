0e335aba2c5396da626495fb3d2b10727987b2c1
Joshua Wang <133073535+Scatter97@users.noreply.github.com>
2026-08-01 19:53:08 -0400

    Add Knightboard Go virtual board foundation

--- app/src/main/java/com/scatter97/chesscamerapgnmobile/MainActivity.kt
+++ app/src/main/java/com/scatter97/chesscamerapgnmobile/MainActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.scatter97.chesscamerapgnmobile.ui.ChessCameraApp
-import com.scatter97.chesscamerapgnmobile.ui.theme.ChessCameraTheme
+import com.scatter97.chesscamerapgnmobile.ui.KnightboardGoApp
+import com.scatter97.chesscamerapgnmobile.ui.theme.KnightboardGoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ChessCameraTheme {
-                ChessCameraApp()
+            KnightboardGoTheme {
+                KnightboardGoApp()
             }
         }
     }
