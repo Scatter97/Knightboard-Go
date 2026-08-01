2f6acfcf485ac64743d1e9332b8fc2f84c8b44cb
Joshua Wang <133073535+Scatter97@users.noreply.github.com>
2026-08-01 19:53:11 -0400

    Add Knightboard Go virtual board foundation

--- app/src/main/java/com/scatter97/chesscamerapgnmobile/ui/ChessCameraApp.kt
+++ app/src/main/java/com/scatter97/chesscamerapgnmobile/ui/ChessCameraApp.kt
@@ -8,16 +8,18 @@ import com.scatter97.chesscamerapgnmobile.ui.calibration.CalibrationScreen
 import com.scatter97.chesscamerapgnmobile.ui.camera.CameraScreen
 import com.scatter97.chesscamerapgnmobile.ui.home.HomeScreen
 import com.scatter97.chesscamerapgnmobile.ui.settings.SettingsScreen
+import com.scatter97.chesscamerapgnmobile.ui.game.VirtualBoardScreen
 
 private object Route {
     const val Home = "home"
     const val Camera = "camera"
     const val Calibration = "calibration"
     const val Settings = "settings"
+    const val VirtualBoard = "virtual-board"
 }
 
 @Composable
-fun ChessCameraApp() {
+fun KnightboardGoApp() {
     val navController = rememberNavController()
 
     NavHost(
@@ -27,6 +29,7 @@ fun ChessCameraApp() {
         composable(Route.Home) {
             HomeScreen(
                 onStartGame = { navController.navigate(Route.Camera) },
+                onOpenVirtualBoard = { navController.navigate(Route.VirtualBoard) },
                 onOpenSettings = { navController.navigate(Route.Settings) },
             )
         }
@@ -46,6 +49,9 @@ fun ChessCameraApp() {
         }
         composable(Route.Settings) {
             SettingsScreen(onBack = { navController.popBackStack() })
+        }
+        composable(Route.VirtualBoard) {
+            VirtualBoardScreen(onBack = { navController.popBackStack() })
         }
     }
 }
